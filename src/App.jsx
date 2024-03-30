c7213b721fb686e348b6db4c5f54211926904af7
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-30 21:48:43 +0530

    XSpellCheck

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import SpellCheckApp from './XSpellCheck'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <SpellCheckApp />
+      </div>
+    </>
+  )
+}
+
+export default App
